c06a855b145c19e61fc6e39351ced72fe547b972
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-02-25 18:59:34 -0500

    Set up dependencies and files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import List from './components/List.jsx';
 import { Dimmer, Loader, Image, Segment, Input } from 'semantic-ui-react';
 import style from '../styles/style.css';
+//
 
 
 class App extends React.Component {
